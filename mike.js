89efb1c39849acb3b146355be2d0234f6aa86164
Justo6 <mikejusto6@gmail.com>
2017-01-20 11:00:40 -0800

    google sign in added, still needs styling

--- mike.js
+++ mike.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
     $('#actionSubmit').click(function(){
         checkForCheckedValues();
-        initMap();
+        initiMapPlaces();
     });
 });
 
 var map;
 var infowindow;
 var checkedBoxes = [];
+
+
+
+// ---------------------------------------Start of google places api---------------------------------------------------
 
 //CheckForCheckedValues takes the values from checked boxes and pushes them to array
 function checkForCheckedValues(){
@@ -17,14 +21,13 @@ function checkForCheckedValues(){
     console.log("users picks to have displayed:" +" "+checkedBoxes);
 }
 
-// initMap HARDCODED irvine as the initial view will need to be changed to users geoLocation
-function initMap() {
+// initiMapPlaces HARDCODED laguna Beach as the initial view will need to be changed to users geoLocation
+function initiMapPlaces() {
     var lagunaBeach = {lat: 33.541679, lng: -117.777214};
     map = new google.maps.Map(document.getElementById('map'), {
         center: lagunaBeach,
         zoom: 12
     });
-
     //The object below is hard coded to show results within the radius if irvine needs to be changed to either   route
     //or to users geoLocation. Name is the values of what will be searched for based off the checked values
     infowindow = new google.maps.InfoWindow();
@@ -32,21 +35,21 @@ function initMap() {
     service.nearbySearch({
         location: lagunaBeach,
         radius: 8046.72, // 5 mile radius search
-        types: checkedBoxes,
-    }, callback);
+        types: checkedBoxes
+    }, callbackPlaces);
 }
 
-//callback function sorts through all the results
-function callback(results, status) {
+//callbackPlaces function sorts through all the results
+function callbackPlaces(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+            createMarkerPlaces(results[i]);
         }
     }
 }
 
-// createMarker places the marker
-function createMarker(place) {
+// createMarkerPlaces places the marker
+function createMarkerPlaces(place) {
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
         map: map,
@@ -59,4 +62,32 @@ function createMarker(place) {
         infowindow.open(map, this);
     });
 }
+//---------------------------------------End of google places api-------------------------------------------------------
 
+
+// client ID:   30315203349-jd80sfb669777fd6d1dfterfccd6c8bq.apps.googleusercontent.com
+// client secret:   klXp63ZEWeJ8DvO7npLy7RvE
+
+//----------------------------------------Start of Google Sign In-------------------------------------------------------
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token); // The ID token you need to pass to your backend:
+};
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+//-----------------------------------------End of Google Sign in--------------------------------------------------------
