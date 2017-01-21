b258a46a61f678b6eaaf7871e724007914b1ffa5
Justo6 <mikejusto6@gmail.com>
2017-01-20 16:54:15 -0800

    sign in and places working

--- mike.js
+++ mike.js
@@ -12,34 +12,29 @@ var checkedBoxes = [];
 
 
 // ---------------------------------------Start of google places api---------------------------------------------------
-
-//CheckForCheckedValues takes the values from checked boxes and pushes them to array
 function checkForCheckedValues(){
     $("input[type=checkbox]:checked").each(function() {
         checkedBoxes.push($(this).val() );
     });
-    console.log("users picks to have displayed:" +" "+checkedBoxes);
+    console.log("users picks to have displayed:" +" "+ checkedBoxes);
 }
 
-// initiMapPlaces HARDCODED laguna Beach as the initial view will need to be changed to users geoLocation
 function initiMapPlaces() {
-    var lagunaBeach = {lat: 33.541679, lng: -117.777214};
+    var Irvine = {lat: 33.6694444, lng: -117.8222222};
     map = new google.maps.Map(document.getElementById('map'), {
-        center: lagunaBeach,
+        center: Irvine,
         zoom: 12
     });
-    //The object below is hard coded to show results within the radius if irvine needs to be changed to either   route
-    //or to users geoLocation. Name is the values of what will be searched for based off the checked values
+
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
-        location: lagunaBeach,
-        radius: 8046.72, // 5 mile radius search
-        types: checkedBoxes
-    }, callbackPlaces);
+        location: Irvine,
+        radius: 8093.4, // 5 mile radius search
+        name: checkedBoxes
+    }, callbackPlaces)
 }
 
-//callbackPlaces function sorts through all the results
 function callbackPlaces(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
@@ -48,7 +43,6 @@ function callbackPlaces(results, status) {
     }
 }
 
-// createMarkerPlaces places the marker
 function createMarkerPlaces(place) {
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
@@ -56,7 +50,6 @@ function createMarkerPlaces(place) {
         position: placeLoc
     });
 
-    // If the marker is clicked the name of that result is shown MAYBE want to add what additional data is displayed
     google.maps.event.addListener(marker, 'click', function() {
         infowindow.setContent(place.name);
         infowindow.open(map, this);
@@ -65,10 +58,9 @@ function createMarkerPlaces(place) {
 //---------------------------------------End of google places api-------------------------------------------------------
 
 
+//----------------------------------------Start of Google Sign In-------------------------------------------------------
 // client ID:   30315203349-jd80sfb669777fd6d1dfterfccd6c8bq.apps.googleusercontent.com
 // client secret:   klXp63ZEWeJ8DvO7npLy7RvE
-
-//----------------------------------------Start of Google Sign In-------------------------------------------------------
 
 function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
@@ -89,5 +81,6 @@ function signOut() {
         console.log('User signed out.');
     });
 }
+
 
 //-----------------------------------------End of Google Sign in--------------------------------------------------------
