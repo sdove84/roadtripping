f993825131378936bd0d743b8ae6f04aa7c38593
Patrick McMahon <pmcmahon14@gmail.com>
2017-01-23 18:05:07 -0800

    added radar request to weather page

--- weather.js
+++ weather.js
@@ -1,28 +1,7 @@
 /**
  * Created by Patrick on 1/17/2017.
  */
-http://icons.wxug.com/i/c/k/chanceflurries.gif
-    http://icons.wxug.com/i/c/k/chancerain.gif
-        http://icons.wxug.com/i/c/k/chancesleet.gif
-            http://icons.wxug.com/i/c/k/chancesnow.gif
-                http://icons.wxug.com/i/c/k/chancetstorms.gif
-                    http://icons.wxug.com/i/c/k/clear.gif
-                        http://icons.wxug.com/i/c/k/cloudy.gif
-                            http://icons.wxug.com/i/c/k/flurries.gif
-                                http://icons.wxug.com/i/c/k/fog.gif
-                                    http://icons.wxug.com/i/c/k/hazy.gif
-                                        http://icons.wxug.com/i/c/k/mostlycloudy.gif
-                                            http://icons.wxug.com/i/c/k/mostlysunny.gif
-                                                http://icons.wxug.com/i/c/k/partlycloudy.gif
-                                                    http://icons.wxug.com/i/c/k/partlysunny.gif
-                                                        http://icons.wxug.com/i/c/k/sleet.gif
-                                                            http://icons.wxug.com/i/c/k/rain.gif
-                                                                http://icons.wxug.com/i/c/k/sleet.gif
-                                                                    http://icons.wxug.com/i/c/k/snow.gif
-                                                                        http://icons.wxug.com/i/c/k/sunny.gif
-                                                                            http://icons.wxug.com/i/c/k/tstorms.gif
-                                                                                http://icons.wxug.com/i/c/k/cloudy.gif
-                                                                                    http://icons.wxug.com/i/c/k/partlycloudy.gif
+
 // trend = result.current_observation.pressure_trend
     if (trend === "+") {
         return(arrow up)
