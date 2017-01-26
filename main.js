3b3675d99fff6b3964045a468368e336746a3af8
Justo6 <mikejusto6@gmail.com>
2017-01-25 18:29:23 -0800

    direct link from landing page to first maps page temporary button to go from 1st maps page to page4 next going to include page4 inside 1st maps page

---
+++ main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Justo on 1/25/17.
+ */
+
+document.getElementById('beginTripButton').addEventListener('click',function(){
+   console.log('test');
+    window.location='autoCompleteDirct.html';
+});
