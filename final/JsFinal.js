688d204e24082eaf389865240bf9cf62c538cf45
Justo6 <mikejusto6@gmail.com>
2017-01-30 15:59:51 -0800

    going to save before styling the maps page and adding ditrections

--- final/JsFinal.js
+++ final/JsFinal.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $("#mode-selector").hide();
     $('#actionSubmit').click(function(){
         checkForCheckedValues();
         startPlaces();
@@ -61,8 +62,8 @@ function AutocompleteDirectionsHandler(map) {
     this.setupClickListener('changemode-driving', 'DRIVING');
     this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
     this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+    this.map.controls[google.maps].push(originInput);
+    this.map.controls[google.maps].push(destinationInput);
     this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
 }
 //     Sets a listener on a radio button to change the filter type on Places
